8ef81ccdc50b5fae9a5a40b1d3e80ba408b7cd51
Jay <WJJay@Jayyyyui-MacBookAir.local>
2022-09-25 15:30:29 +0900

    wip

---
+++ api/controllers/cartsControllers.js
@@ -0,0 +1,6 @@
+const { cartsServiece } = require('../services')
+const {asyncWrap} = require('../utils/error')
+
+const cartsPost = asyncWrap(async (req, res) => {
+    const {}
+})
---
+++ api/routes/cartsRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { cartsControllers } = require('../controllers');
+
+const router = express.Router();
+
+router.post('/', cartsControllers.cartsGet)
+
+module.exports = router;
