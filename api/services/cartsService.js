f73d94f3e605b2c8816a138787bd37e62972562a
ChooJaeHo <choojaeho@gmail.com>
2022-09-26 01:38:53 +0900

    삭제할 게시물을 선택하지 않았을때 예외처리

--- api/services/cartsService.js
+++ api/services/cartsService.js
@@ -1,7 +1,13 @@
 const { cartsDao } = require('../models')
 
 const  deleteCart= async(userId, cartId) =>{
-        return await cartsDao.deleteCheckCart(userId, cartId)
+        const deleteCart = await cartsDao.deleteCheckCart(userId, cartId)
+        if (deleteCart == 0){
+            const err = new Error('NO_CARTS_TO_DELETE')
+            err.statusCode = 404;
+            
+            throw err;
+        }
 }
 
 module.exports ={ 
