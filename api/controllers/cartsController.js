cf799efd84555e180152c127769606e0b13c6eed
ChooJaeHo <choojaeho@gmail.com>
2022-09-26 17:49:44 +0900

    delete for문 삭제 쿼리문 in 추가

--- api/controllers/cartsController.js
+++ api/controllers/cartsController.js
@@ -5,12 +5,6 @@ const deleteCart = asyncWrap(async (req,res) => {
     const userId = req.user.id
     const cartIds = req.query.cartId
     
-    // if(Array.isarray(cartId)){
-    //     for(let i=0; i<cartId.length; i++){
-    //     await cartsService.deleteCart(userId, +cartId[i])
-    // }}else{
-    //     await cartsService.deleteCart(userId, +cartId)
-    // }
     await cartsService.deleteCart(userId, cartIds)
     res.status(201).json({messge : "SUCCESS_DELETE_CART"})
 });
