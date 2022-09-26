04837030ffa5fa47f00ee2f210df2d8059528b9e
ChooJaeHo <choojaeho@gmail.com>
2022-09-26 17:36:08 +0900

    delete 수정중

--- api/controllers/cartsController.js
+++ api/controllers/cartsController.js
@@ -3,14 +3,15 @@ const { asyncWrap } = require('../utils/error')
 
 const deleteCart = asyncWrap(async (req,res) => {
     const userId = req.user.id
-    const cartId = req.query.cartId
+    const cartIds = req.query.cartId
     
-    if(Array.isarray(cartId)){
-        for(let i=0; i<cartId.length; i++){
-        await cartsService.deleteCart(userId, +cartId[i])
-    }}else{
-        await cartsService.deleteCart(userId, +cartId)
-    }
+    // if(Array.isarray(cartId)){
+    //     for(let i=0; i<cartId.length; i++){
+    //     await cartsService.deleteCart(userId, +cartId[i])
+    // }}else{
+    //     await cartsService.deleteCart(userId, +cartId)
+    // }
+    await cartsService.deleteCart(userId, cartIds)
     res.status(201).json({messge : "SUCCESS_DELETE_CART"})
 });
 
