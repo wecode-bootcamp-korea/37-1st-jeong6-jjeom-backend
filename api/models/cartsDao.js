42bb676284fb9e8f500b52959e5aeec62535c164
ChooJaeHo <choojaeho@gmail.com>
2022-09-26 18:19:44 +0900

    Delete Cart for문 삭제/쿼리 in 활용/ DAO 에러헨들링 변경

--- api/models/cartsDao.js
+++ api/models/cartsDao.js
@@ -1,20 +1,24 @@
 const appDataSource  =require('./datasource')
 
-const deleteCheckCart = async(userId, cartId) =>{
+const deleteCheckCart = async(userId, cartIds) =>{
     
     const deleteCartRows = (await appDataSource.query( 
         `
             DELETE FROM carts
             Where carts.users_id =?
             AND carts.id in (?)
-        `, [userId, cartId]
+        `, [userId, cartIds]
         )).affectedRows
-            
-    if (deleteCartRows !==0 && deleteCartRows !==1){
-    throw new Error ('UNEXPECTED_NUMBER_OF_RECORDS_DELETED')}
-
-return deleteCartRows
-}
+    
+    if (Array.isArray(cartIds)) {
+        if (deleteCartRows !== cartIds.length){
+            throw new Error ('UNEXPECTED_NUMBER_OF_RECORDS_DELETED')}
+    } else {
+        if (![0, 1].includes(deleteCartRows)) {
+            throw new Error ('UNEXPECTED_NUMBER_OF_RECORDS_DELETED')}
+        }
+        return deleteCartRows
+    }
 
 module.exports = { 
   deleteCheckCart
--- api/services/cartsService.js
+++ api/services/cartsService.js
@@ -1,7 +1,7 @@
 const { cartsDao } = require('../models')
 
-const  deleteCart= async(userId, cartId) =>{
-        const deleteCart = await cartsDao.deleteCheckCart(userId, cartId)
+const  deleteCart= async(userId, cartIds) =>{
+        const deleteCart = await cartsDao.deleteCheckCart(userId, cartIds)
         if (deleteCart == 0){
             const err = new Error('NO_CARTS_TO_DELETE')
             err.statusCode = 404;
