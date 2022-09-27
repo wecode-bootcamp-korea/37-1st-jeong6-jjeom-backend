820fd81f9a149f34554d6d7d43beaa74248ad0d3
Jay <WJJay@Jayyyyui-MacBookAir.local>
2022-09-27 21:22:07 +0900

    결제 db 생성

---
+++ db/migrations/20220927025305_delivery_information.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+CREATE TABLE delivery_information (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    arrival_date VARCHAR(50) NOT NULL,
+    delivery_method BOOLEAN NOT NULL,
+    users_id int NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT delivery_information_users_id_fkey FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+-- migrate:down
+DROP TABLE delivery_information;
